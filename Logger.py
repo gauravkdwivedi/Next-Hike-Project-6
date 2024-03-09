2361ddc5ee23861aa6e94495f0d3a2e6407fa672
gauravkdwivedi <gauravdwivedi91@hotmail.com>
2024-03-09 22:00:11 +0530

    Data Preprocessing

--- Logger.py
+++ Logger.py
@@ -1,23 +1,3 @@
-# Import the logger library
-import logging
-
-# Configure the login system you are using
-logging.basicConfig(level=logging.DEBUG, # sets the login level
-                    format='%(asctime)s - %(levelname)s - %(message)s')
-
-# Create a logger
-logger = logging.getLogger(__name__)
-
-# Log messages at different levels
-logger.debug('This is a debug message')
-logger.info('This is an info message')
-logger.warning('This is a warning message')
-logger.error('This is an error message')
-logger.critical('This is a critical message')
-
-# Log an exception with stack trace
-try:
-    # Code that might raise an exception
-    result = 1 / 0
-except Exception as e:
-    logger.exception("An exception occurred: %s", e)
+version https://git-lfs.github.com/spec/v1
+oid sha256:f2ebbd3252429464090874a905ecc9aea68d92e0fa2dfe8bca39f31f717ff03f
+size 724
